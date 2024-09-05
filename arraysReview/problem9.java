40cc77de9dc1739499d85bdba45b9084c6411a31
Braeden Audlin <125329807+25Braeden@users.noreply.github.com>
2024-09-05 23:21:16 +0000

    whoop whoop

--- arraysReview/problem9.java
+++ arraysReview/problem9.java
@@ -3,17 +3,34 @@ import java.util.Random;
 public class problem9 {
     public static void main(String[] args) {
         ArrayList<Integer> myArray = new ArrayList<>();
+        ArrayList<Integer> numsToFour = new ArrayList<>();
         Random random = new Random();
-        BubbleSort bubbleSort = new BubbleSort();
-        for(int i = 0; i < 10; i ++) {
+
+        int indexFour = -1;
+
+        for (int i = 0; i < 10; i ++) {
             if(i < 5) {
-                myArray.add(random.nextInt(4));
-            } else if (
-                
-            )
-             else {
-                myArray.add(random.nextInt(10));
+                myArray.add(random.nextInt(5));
+            }else {
+                myArray.add(random.nextInt(i + 1));
             }
+        }
+        for (int i = 0; i < myArray.size() - 1; i++) {
+            if (myArray.get(i) == 4) {
+                indexFour = i;
+                break;
+            }
+        }
+        
+        if (indexFour != -1) {
+            System.out.println(myArray);
+            for (int i = 0; i < indexFour; i++) {
+                numsToFour.add(myArray.get(i));
+            }
+            System.out.println(numsToFour);
+        } else {
+            System.out.println(myArray);
+            System.out.println("No four in array.");
         }
 
     }
